f69b2837619938d06732657d3b7979f75775e173
= <=>
2021-12-02 14:01:49 +0300

    Creating the structure of the project

---
+++ CameraApp/CameraApp/LogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogInViewController.swift
+//  CameraApp
+//
+//  Created by Rayan Taj on 02/12/2021.
+//
+
+import UIKit
+// AHMED 
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CameraApp/CameraApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by Rayan Taj on 02/12/2021.
+//
+
+import UIKit
+
+//  DO NOT WRITE ANY CODE
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        
+        
+    }
+
+
+}
+
