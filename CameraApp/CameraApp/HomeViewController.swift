17bfc47b0bdc566a62252b2dab9cd1ffbeb8fde4
ibrahim <ibrahim.hosainan.ruh.s@tuwaiq.edu.sa>
2021-12-02 16:32:43 +0300

    ibrahimhomViweControloler

--- CameraApp/CameraApp/HomeViewController.swift
+++ CameraApp/CameraApp/HomeViewController.swift
@@ -10,21 +10,66 @@ import UIKit
 // IBRA
 class HomeViewController: UIViewController {
 
+    var homeStackView = UIStackView()
+    
+    var userNameLable = UILabel()
+    
+    var cameraButton = UIButton()
+    
+    var imageViewObject = UIImageView()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        
+        userNameLable.frame = CGRect(x:100, y: 60, width: 200, height: 40)
+        userNameLable.backgroundColor = .white
+        userNameLable.text = "Hello Ahmed"
+        userNameLable.textAlignment = .center
+        
+        
+        
+        imageViewObject = UIImageView(frame:CGRect(x: 0, y: 100, width: 400, height: 600))
+       
+        
+        imageViewObject.image = UIImage(named:"bb")
+      
 
-        // Do any additional setup after loading the view.
+        self.view.addSubview(imageViewObject)
+        
+        
+        
+        
+        
+        
+        
+        
+        cameraButton.frame = CGRect(x:70, y: 800, width: 300, height: 40)
+        cameraButton.setTitle("login", for: .normal)
+        cameraButton.addTarget(self, action: #selector(cameraButtonTapped), for: .touchDown)
+        cameraButton.backgroundColor = .blue
+        
+        
+        
+        view.addSubview(userNameLable)
+        view.addSubview(cameraButton)
+    }
+    
+    
+    @objc func cameraButtonTapped(){
+        
+        
+        print("iiii")
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
